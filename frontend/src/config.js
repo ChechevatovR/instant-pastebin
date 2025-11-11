fdb33dea5a29b0e302a9ea0cd54a64430c0fa136
Darknessich <blakibrown@gmail.com>
2025-11-11 21:04:00 +0300

    frontend

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const BACKEND_BASE = '' // e.g., 'http://255.255.255.255'
+export const STUN_SERVERS = [{ urls: ['stun:stun.l.google.com:19302'] }]
+export const CHUNK_SIZE = 64 * 1024 // 64KB
